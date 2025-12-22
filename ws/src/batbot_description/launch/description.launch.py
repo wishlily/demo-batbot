cd953954a338f51f0693fa220d91ad9662dbf6fc
emily <hebut_jl@hotmail.com>
2025-12-22 16:55:32 +0800

    ✨ feat(ws): BatBot Description
    1. Add gazebo plugin

---
+++ ws/src/batbot_description/launch/description.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("batbot_description")
+    model_arg = DeclareLaunchArgument(
+        "model",
+        default_value=os.path.join(pkg_share, "urdf/batbot", "batbot.urdf.xacro"),
+        description="Path to robot description file (URDF or XACRO)",
+    )
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), value_type=str
+    )
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+    )
+
+    joint_state_publisher = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+    )
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher,
+        joint_state_publisher
+    ])
