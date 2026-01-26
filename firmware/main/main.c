16c24a2e265a20d01a28ad9310192ae02d93af61
emily <hebut_jl@hotmail.com>
2026-01-26 17:13:15 +0800

    ✨ feat(fmw): micro-ROS
    1. Add odom pub

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -20,6 +20,7 @@
 #include <sensor_msgs/msg/imu.h>
 #include <sensor_msgs/msg/laser_scan.h>
 #include <std_msgs/msg/int32.h>
+#include <nav_msgs/msg/odometry.h>
 #include <uros_network_interfaces.h>
 
 #ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
@@ -70,7 +71,7 @@ static const char* TAG = "MAIN";
 
 static uint64_t time_offset_us = 0; // us
 static uint64_t last_sync_time = 0; // us
-static volatile uint16_t pub_bad_count = 0;
+static uint64_t last_ping_time = 0; // us
 
 static void sync_time(const int timeout_ms)
 {
@@ -129,22 +130,15 @@ static void imu_data_init(void)
 
 void imu_timer_publisher(rcl_timer_t* timer, int64_t last_call_time)
 {
+    RCLC_UNUSED(last_call_time);
+    static int err_count = 0;
     imu_data_t data;
     for (int i = 0; i < 5; i++) {
         int rc = imu_wait_for_data(&data, 0);
         if (rc) {
             break;
         }
-        // static int count = 0;
-        // if (count++ % 50 == 0) {
-        //     ESP_LOGI(
-        //         TAG,
-        //         "%5llu us, Accel: % 8.2f % 8.2f % 8.2f m/s^2, Gyro: % 8.2f % 8.2f % 8.2f, Temp: % 4.2f
-        //         degC", data->timestamp, data->accel_x, data->accel_y, data->accel_z, data->gyro_x,
-        //         data->gyro_y,
-        //         data->gyro_z,
-        //         imu_get_temperature());
-        // }
+
         imu_msg.linear_acceleration.x = data.accel_x;
         imu_msg.linear_acceleration.y = data.accel_y;
         imu_msg.linear_acceleration.z = data.accel_z;
@@ -158,10 +152,9 @@ void imu_timer_publisher(rcl_timer_t* timer, int64_t last_call_time)
 
         rc = rcl_publish(&imu_publisher, &imu_msg, NULL);
         if (rc != RCL_RET_OK) {
-            pub_bad_count++;
-            ESP_LOGE(TAG, "Failed to publish IMU message: %d", pub_bad_count);
+            ESP_LOGE(TAG, "Failed to publish IMU message: %d", err_count++);
         } else {
-            pub_bad_count = 0;
+            err_count = 0;
         }
     }
 }
@@ -202,6 +195,8 @@ static esp_err_t lidar_data_init(void)
 
 void lidar_timer_publisher(rcl_timer_t* timer, int64_t last_call_time)
 {
+    RCLC_UNUSED(last_call_time);
+    static int err_count = 0;
     ms200_frame_t frame;
     for (int retry = 0; retry < 3; retry++) { // try 3 times
         int rc = lidar_ms200_read(&frame, 0);
@@ -225,18 +220,115 @@ void lidar_timer_publisher(rcl_timer_t* timer, int64_t last_call_time)
 
         rc = rcl_publish(&lidar_publisher, &lidar_msg, NULL);
         if (rc != RCL_RET_OK) {
-            pub_bad_count++;
-            ESP_LOGE(TAG, "Failed to publish Laser message: %d", pub_bad_count);
+            ESP_LOGE(TAG, "Failed to publish Laser message: %d", err_count++);
         } else {
-            pub_bad_count = 0;
+            err_count = 0;
         }
     }
 }
 
-static void data_init(void)
+static nav_msgs__msg__Odometry odom_msg;
+static rcl_publisher_t odom_publisher;
+
+static void odom_data_init(void)
+{
+    nav_msgs__msg__Odometry__init(&odom_msg);
+    rosidl_runtime_c__String__assign(&odom_msg.header.frame_id, "odom_frame");
+    rosidl_runtime_c__String__assign(&odom_msg.child_frame_id, "base_footprint");
+
+    for (int i = 0; i < 36; i++) {
+        odom_msg.pose.covariance[i] = 0.0;
+        odom_msg.twist.covariance[i] = 0.0;
+    }
+    odom_msg.pose.covariance[0] = 0.01;  // x
+    odom_msg.pose.covariance[7] = 0.01;  // y
+    odom_msg.pose.covariance[35] = 0.01; // yaw (approx)
+
+    odom_msg.twist.covariance[0] = 0.01;
+    odom_msg.twist.covariance[7] = 0.01;
+    odom_msg.twist.covariance[35] = 0.01;
+
+    odom_publisher = rcl_get_zero_initialized_publisher();
+}
+
+// only use yaw
+static void odom_euler_to_quat(float yaw, geometry_msgs__msg__Quaternion* q)
+{
+    const float cos_0 = cos(0);
+    const float sin_0 = sin(0);
+    float cy = cos(yaw * 0.5);
+    float sy = sin(yaw * 0.5);
+    float cp = cos_0; // cos(pitch * 0.5)
+    float sp = sin_0; // sin(pitch * 0.5)
+    float cr = cos_0; // cos(roll * 0.5)
+    float sr = sin_0; // sin(roll * 0.5)
+
+    q->x = cy * cp * cr + sy * sp * sr;
+    q->y = cy * cp * sr - sy * sp * cr;
+    q->z = sy * cp * sr + cy * sp * cr;
+    q->w = sy * cp * cr - cy * sp * sr;
+}
+
+void odom_timer_publisher(rcl_timer_t* timer, int64_t last_call_time)
+{
+    RCLC_UNUSED(last_call_time);
+    static int err_count = 0;
+    static uint64_t last_time = 0;
+    uint64_t now = esp_timer_get_time();
+    if (last_time == 0) {
+        last_time = now; // first skip
+        return;
+    }
+    float dt = (now - last_time) / 1000000.0f;
+    last_time = now;
+    if (dt < 0)
+        dt = 0;
+
+    motion_cmd_t speed;
+    motion_get_speed(&speed);
+
+    static float x = 0;
+    static float y = 0;
+    static float yaw = 0;
+
+    float cos_yaw = cos(yaw);
+    float sin_yaw = sin(yaw);
+    float delta_x = (speed.Vx * cos_yaw - speed.Vy * sin_yaw) * dt; // m
+    float delta_y = (speed.Vx * sin_yaw + speed.Vy * cos_yaw) * dt; // m
+
+    x += delta_x;
+    y += delta_y;
+    yaw += speed.Wz * dt;
+
+    odom_msg.pose.pose.position.x = x;
+    odom_msg.pose.pose.position.y = y;
+    odom_msg.pose.pose.position.z = 0;
+    odom_euler_to_quat(yaw, &odom_msg.pose.pose.orientation);
+
+    odom_msg.twist.twist.linear.x = speed.Vx;
+    odom_msg.twist.twist.linear.y = speed.Vy;
+    odom_msg.twist.twist.angular.z = speed.Wz;
+
+    uint64_t sys_us = now + time_offset_us;
+    odom_msg.header.stamp.sec = sys_us / 1000000;
+    odom_msg.header.stamp.nanosec = (sys_us % 1000000) * 1000;
+    rcl_ret_t rc = rcl_publish(&odom_publisher, &odom_msg, NULL);
+    if (rc != RCL_RET_OK) {
+        ESP_LOGE(TAG, "Failed to publish Odom messages: %d", err_count++);
+    } else {
+        err_count = 0;
+    }
+}
+
+static esp_err_t data_init(void)
 {
     imu_data_init();
-    lidar_data_init();
+    esp_err_t rc = lidar_data_init();
+    if (rc != ESP_OK) {
+        return rc;
+    }
+    odom_data_init();
+    return ESP_OK;
 }
 
 #define RC_GOTO(fn, label, msg)                                                                      \
@@ -272,6 +364,7 @@ void micro_ros_task(void* arg)
 
     rcl_timer_t timer_imu;
     rcl_timer_t timer_lidar;
+    rcl_timer_t timer_odom;
 
     while (1) {
         init_options = rcl_get_zero_initialized_init_options();
@@ -312,6 +405,7 @@ void micro_ros_task(void* arg)
         executor = rclc_executor_get_zero_initialized_executor();
         timer_imu = rcl_get_zero_initialized_timer();
         timer_lidar = rcl_get_zero_initialized_timer();
+        timer_odom = rcl_get_zero_initialized_timer();
         memset(&support, 0, sizeof(rclc_support_t));
 
         if (rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator) != RCL_RET_OK) {
@@ -339,7 +433,7 @@ void micro_ros_task(void* arg)
                 cleanup,
                 "Failed to init imu publisher");
         RC_GOTO(rclc_timer_init_default2(&timer_imu, &support, RCL_MS_TO_NS(10), imu_timer_publisher, true),
-                cleanup,
+                cleanup, // 100Hz
                 "Failed to init imu timer");
 
         RC_GOTO(
@@ -349,11 +443,19 @@ void micro_ros_task(void* arg)
             "Failed to init laser publisher");
         RC_GOTO(
             rclc_timer_init_default2(&timer_lidar, &support, RCL_MS_TO_NS(100), lidar_timer_publisher, true),
-            cleanup,
+            cleanup, // 10Hz
             "Failed to init laser timer");
 
-        // 2 Subscription + 2 Timer
-        RC_GOTO(rclc_executor_init(&executor, &support.context, 4, &allocator),
+        RC_GOTO(rclc_publisher_init_default(
+                    &odom_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry), "odom_raw"),
+                cleanup,
+                "Failed to init odom publisher");
+        RC_GOTO(rclc_timer_init_default2(&timer_odom, &support, RCL_MS_TO_NS(50), odom_timer_publisher, true),
+                cleanup, // 20Hz
+                "Failed to init odom timer");
+
+        // 2 Subscription + 3 Timer
+        RC_GOTO(rclc_executor_init(&executor, &support.context, 5, &allocator),
                 cleanup,
                 "Failed to init executor");
         RC_GOTO(rclc_executor_add_subscription(
@@ -366,30 +468,29 @@ void micro_ros_task(void* arg)
                 "Failed to add twist subscriber");
         // RC_GOTO(rclc_executor_add_timer(&executor, &timer_imu), cleanup, "Failed to add imu timer");
         // RC_GOTO(rclc_executor_add_timer(&executor, &timer_lidar), cleanup, "Failed to add lidar timer");
+        RC_GOTO(rclc_executor_add_timer(&executor, &timer_odom), cleanup, "Failed to add odom timer");
 
         sync_time(1000);
 
         ESP_LOGI(TAG, "Micro-ROS task running!");
-        pub_bad_count = 0;
         while (1) {
             if (rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)) != RCL_RET_OK) {
                 ESP_LOGI(TAG, "Connection lost/Error in spin. Resetting...");
                 break;
             }
-            if ((esp_timer_get_time() - last_sync_time) > 300000000LL) { // 5 minutes
+            uint64_t now = esp_timer_get_time();
+            if ((now - last_sync_time) > 300000000LL) { // 5 minutes
                 sync_time(100);
-                last_sync_time = esp_timer_get_time();
+                last_sync_time = now;
             }
-            if (rmw_uros_ping_agent(100, 1) != RCL_RET_OK) {
-                ESP_LOGW(TAG, "Agent ping failed, resetting...");
-                break;
+            if ((now - last_ping_time) > 5000000) { // 5 seconds
+                if (rmw_uros_ping_agent(100, 1) != RCL_RET_OK) {
+                    ESP_LOGW(TAG, "Agent ping failed, resetting...");
+                    break;
+                }
+                last_ping_time = now;
             }
-            if (pub_bad_count > 10) {
-                ESP_LOGW(TAG, "Failed to publish IMU message %d times", pub_bad_count);
-                pub_bad_count = 0;
-                break;
-            }
-            vTaskDelay(pdMS_TO_TICKS(10));
+            vTaskDelay(pdMS_TO_TICKS(1));
         }
     cleanup:
         ESP_LOGI(TAG, "Cleaning up resources...\n");
@@ -397,8 +498,11 @@ void micro_ros_task(void* arg)
         RCSOFTCHECK(rcl_subscription_fini(&subscriber_test, &node));
         RCSOFTCHECK(rcl_subscription_fini(&subscriber_twist, &node));
         RCSOFTCHECK(rcl_publisher_fini(&imu_publisher, &node));
+        RCSOFTCHECK(rcl_publisher_fini(&lidar_publisher, &node));
+        RCSOFTCHECK(rcl_publisher_fini(&odom_publisher, &node));
         RCSOFTCHECK(rcl_timer_fini(&timer_imu));
         RCSOFTCHECK(rcl_timer_fini(&timer_lidar));
+        RCSOFTCHECK(rcl_timer_fini(&timer_odom));
         RCSOFTCHECK(rcl_node_fini(&node));
         RCSOFTCHECK(rclc_support_fini(&support));
         RCSOFTCHECK(rcl_init_options_fini(&init_options));
@@ -412,7 +516,7 @@ void micro_ros_task(void* arg)
 
 void app_main(void)
 {
-    data_init();
+    ESP_ERROR_CHECK(data_init());
 
     app_state_init();
     // ESP_ERROR_CHECK_WITHOUT_ABORT(imu_init());
